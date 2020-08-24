0103e984582178660797f4e6b25e18fa78b135ad
immer_000 <immerhjw@gmail.com>
2020-08-24 17:30:21 +0900

    test-helloworld

---
+++ cmake-build-debug/CMakeFiles/scoringC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scoringC.dir/main.cpp.obj"
+  "libscoringC.dll.a"
+  "scoringC.exe"
+  "scoringC.exe.manifest"
+  "scoringC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scoringC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by immer_000 on 2020-08-24.
+//
+
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    cout<<"hello world"<<endl;
+    return 0;
+}
