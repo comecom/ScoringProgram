f46e7b7a76423268fd56fe32ab24674513636ec6
immer_000 <immerhjw@gmail.com>
2020-08-24 21:10:50 +0900

    Execute python file without input

--- main.cpp
+++ main.cpp
@@ -3,10 +3,20 @@
 //
 
 #include<iostream>
+#include<string>
 
 using namespace std;
+
 int main()
 {
-    cout<<"hello world"<<endl;
+    string location = "C:/Users/immer_000/Desktop/test/";
+    string filename = "practice1.py";
+    string command = "python ";
+
+    string filepath = location + filename; //filepath store
+    command += filepath;    //python test/practice1.py
+
+    system(command.c_str()); //string -> char* (translation)
+
     return 0;
 }
